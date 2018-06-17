0d6a2d4b691174a69cc7e49f465500b3816b5d7d
mark guindi <>
2018-06-17 00:29:33 -0400

    Move setting to secret_settings

--- job_application_manager/settings.py
+++ job_application_manager/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@ysx^kv&&4kxep@k960**en4bgvr)ji%_icg1st42)lvs57a1&'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
