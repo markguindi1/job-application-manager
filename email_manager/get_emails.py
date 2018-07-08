0cc105ffceb022e0c711f9c1e0c357330b783631
mark guindi <>
2018-07-08 02:22:11 -0400

    Quick fix for case where there are no emails (catch IndexError)

--- email_manager/get_emails.py
+++ email_manager/get_emails.py
@@ -16,56 +16,60 @@ class Email:
 # %d-%b-%Y
 # date format: 17-Jun-2018
 def get_emails(gmail_email, pswd, since_date):
-
+    
     emails_list = []
-    DATE_FORMAT = "%d-%b-%Y"
 
+    try:
+        DATE_FORMAT = "%d-%b-%Y"
 
-    # 'mail' is the server
-    mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+        # 'mail' is the server
+        mail = imaplib.IMAP4_SSL(SMTP_SERVER)
 
-    # login using email & pswd
-    mail.login(gmail_email, pswd)
+        # login using email & pswd
+        mail.login(gmail_email, pswd)
 
-    # select only messages with 'inbox' tag
-    mail.select('inbox')
+        # select only messages with 'inbox' tag
+        mail.select('inbox')
 
-    # 1st mandatory param is charset (None in this case), 2nd is search criteria
-    # atype is a str with some 'status', in this case its value is "OK"
-    # data is a list with only one element, a series of bytes
-    # atype, data = mail.search(None, 'ALL')
-    since_date = since_date.strftime(DATE_FORMAT)
-    atype, data = mail.search(None, 'SINCE "{}"'.format(since_date))
+        # 1st mandatory param is charset (None in this case), 2nd is search criteria
+        # atype is a str with some 'status', in this case its value is "OK"
+        # data is a list with only one element, a series of bytes
+        # atype, data = mail.search(None, 'ALL')
+        since_date = since_date.strftime(DATE_FORMAT)
+        atype, data = mail.search(None, 'SINCE "{}"'.format(since_date))
 
+        # mail_ids is a series of bytes
+        mail_ids = data[0]
 
-    # mail_ids is a series of bytes
-    mail_ids = data[0]
+        # splits into ***strings*** using sep=(ASCII whitespace)
+        id_list = mail_ids.split()
 
-    # splits into ***strings*** using sep=(ASCII whitespace)
-    id_list = mail_ids.split()
+        earlier_email_id = int(id_list[0])
+        latest_email_id = int(id_list[-1])
 
-    earlier_email_id = int(id_list[0])
-    latest_email_id = int(id_list[-1])
+        # for the id of emazils received since the specified date
+        for i in range(latest_email_id, earlier_email_id, -1):
 
-    # for the id of emazils received since the specified date
-    for i in range(latest_email_id, earlier_email_id, -1):
+            # typ is some status code, like above. "OK" in this case.
+            # data is a list of two objects:
+            # 1. A tuple with bytes of the relevant data we want
+            # 2. some irrelevant byte object
+            typ, data = mail.fetch(str(i), '(RFC822)')
 
-        # typ is some status code, like above. "OK" in this case.
-        # data is a list of two objects:
-        # 1. A tuple with bytes of the relevant data we want
-        # 2. some irrelevant byte object
-        typ, data = mail.fetch(str(i), '(RFC822)')
+            # We only care about the data's tuple in the below loop
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    msg = email.message_from_bytes(response_part[1])
 
-        # We only care about the data's tuple in the below loop
-        for response_part in data:
-            if isinstance(response_part, tuple):
-                msg = email.message_from_bytes(response_part[1])
+                    email_id = i
+                    email_subject = msg['subject']
+                    email_from = msg['from']
+                    email_date = msg['date']
+                    an_email = Email(email_id, email_from, email_date, email_subject)
+                    emails_list.append(an_email)
 
-                email_id = i
-                email_subject = msg['subject']
-                email_from = msg['from']
-                email_date = msg['date']
-                an_email = Email(email_id, email_from, email_date, email_subject)
-                emails_list.append(an_email)
+    # If there are no emails (quick fix - to do proper fix later)
+    except IndexError as e:
+        pass
 
     return emails_list
