db06be740deeb859da060dd6be6aa693e3b8128e
mark guindi <>
2018-07-15 14:06:40 -0400

    Prevent users from viewing resources not owned by them

--- application_manager/views.py
+++ application_manager/views.py
@@ -10,24 +10,27 @@ from .forms import *
 from .tables import *
 # Create your views here.
 
-APPLICATION_FORM_FIELDS = ['company_name',
-          'position',
-          'position_type',
-          'city',
-          'state',
-          'country',
-          'on_site',
-          'application_link',
-          'status',
-          'lead'
+
+APPLICATION_FORM_FIELDS = [
+        'company_name',
+        'position',
+        'position_type',
+        'city',
+        'state',
+        'country',
+        'on_site',
+        'application_link',
+        'status',
+        'lead'
 ]
+
 
 class HomepageView(TemplateView):
     template_name = "application_manager/base.html"
 
 
 # This class contains the model name, template name, and context_object_name.
-# It's queryset is automatically set to all instances of its model, and it returns
+# It's queryset is all the user's job applications (default is all instances of its model), and it returns
 # an HttpResponse with all of the instances of its model
 class ApplicationsListView(LoginRequiredMixin, ListView):
     model = Application
@@ -47,7 +50,7 @@ class ApplicationCreate(LoginRequiredMixin, CreateView):
     fields = APPLICATION_FORM_FIELDS
     template_name_suffix = '-form'
 
-    # Overriden in order to set the new application's user to the current user
+    # Overridden in order to set the new application's user to the current user
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
@@ -62,6 +65,10 @@ class ApplicationUpdate(LoginRequiredMixin, UpdateView):
     fields = APPLICATION_FORM_FIELDS
     template_name_suffix = '-form'
 
+    # Overridden in order to confirm that the request is coming from the owner of this resource
+    def get_queryset(self):
+        return super(ApplicationUpdate, self).get_queryset().filter(user=self.request.user)
+
 
 # when request.method == GET, accepts pk parameter, and returns HttpResponse of template containing
 # confirmation for deletion of its object.
@@ -72,3 +79,7 @@ class ApplicationDelete(LoginRequiredMixin, DeleteView):
     fields = APPLICATION_FORM_FIELDS
     template_name_suffix = '-delete-form'
     success_url = reverse_lazy('application_manager:application-list')
+
+    # Overridden in order to confirm that the request is coming from the owner of this resource
+    def get_queryset(self):
+        return super(ApplicationDelete, self).get_queryset().filter(user=self.request.user)
